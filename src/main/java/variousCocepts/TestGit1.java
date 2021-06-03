06d9228376a79d0bbcffbbec8c27f5e04f6d2202
SupPonnala <supritha.ponnala@gmail.com>
2021-06-03 17:07:59 -0500

    third commit

---
+++ src/main/java/variousCocepts/TestGit1.java
@@ -0,0 +1,12 @@
+package variousCocepts;
+
+public class TestGit1 {
+	public static void main(String[] args) {
+		printMe1();
+	}
+public static void printMe1() {
+	
+	System.out.println("Test Git1");
+}
+
+}
